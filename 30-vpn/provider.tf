621250133b4833094c22f43091e51666aee4fbda
mano jyoti <manojyoti434@gmail.com>
2025-07-28 20:13:57 +0530

    aws terraform roboshop

--- 30-vpn/provider.tf
+++ 30-vpn/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "mano-remotestate-dev"
-    key    = "roboshop-dev-sg"
+    key    = "roboshop-dev-vpn"
     region = "us-east-1"
     encrypt        = true
     use_lockfile = true
